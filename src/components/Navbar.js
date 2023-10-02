0e08d55e36d5208144b5e42f189f7c25ddeebf19
husseinhareb <shtam.hareb12@gmail.com>
2023-10-02 22:59:35 +0300

    nav edit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import logo from "../Airbnb-logo.png"
+import "../../src/index.css"
+export default function Navbar(){
+    return(
+        <nav>
+            <img className="nav-logo" src={logo} />
+        </nav>
+    )
+}
