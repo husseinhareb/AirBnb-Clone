f39397dd12fb3bdf1fc8006c5e1d82ccfb1acea6
husseinhareb <shtam.hareb12@gmail.com>
2023-10-02 23:21:13 +0300

    photo grid

---
+++ src/components/Grid.js
@@ -0,0 +1,10 @@
+import react from "react"
+import grid from "../photo-grid.png"
+
+export default function Grid(){
+    return(
+        <div>
+            <img src={grid} />
+        </div>
+    )
+}
