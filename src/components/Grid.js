e26b0695e776d9893708f69952ed5c1087d9ea9f
husseinhareb <shtam.hareb12@gmail.com>
2023-10-17 18:50:32 +0300

    changes to Grid.js file

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,14 @@
 import react from "react"
 import grid from "../photo-grid.png"
-
+import "../../src/index.css"
 export default function Grid(){
     return(
-        <div>
-            <img src={grid} />
-        </div>
+        <section className="section">
+            <img className="grid" src={grid} />
+            <h1 className="header">Online Experiences</h1>
+            <p className="text">Join unique interactive activities led by
+                one-of-a-kind hosts-all without leaving 
+                home</p> 
+        </section>
     )
 }
