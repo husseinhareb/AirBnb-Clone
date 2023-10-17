42adb7b0a019f335de19eb70b8ad2cd3f51ada8d
husseinhareb <shtam.hareb12@gmail.com>
2023-10-17 18:49:30 +0300

    card js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,15 @@
 import react from "react"
 import katie from "../katie-zaferes.png"
 import star from "../star.png"
+import "../../src/index.css"
+
 export default function Card(){
     return(
-        <div>
-            <img src={katie} />
-            <img src={star} />
-            <p>Life lessons with Katie Zaferes</p>
+        <div className="card">
+            <img className="katie" src={katie} />
+            <img className="star" src={star} />
+            <p className="rating"> 5.0 (6).USA</p>
+            <p className="description">Life lessons with Katie Zaferes</p>
         </div>
     )
 }
