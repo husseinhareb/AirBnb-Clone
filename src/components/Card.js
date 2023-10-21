b34cf7562d2d7dfa69225ceb9ec3eef044873ef4
husseinhareb <shtam.hareb12@gmail.com>
2023-10-21 13:37:39 +0300

    learning props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,19 @@
 import react from "react"
-import katie from "../images/katie-zaferes.png"
-import star from "../images/star.png"
+
 import "../../src/index.css"
 
 export default function Card(props){
-    console.log(props)
     return(
         <div className="card">
          <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
