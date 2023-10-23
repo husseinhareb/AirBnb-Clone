6b75e5fd26923e530881e3d9e8168bac067bce49
husseinhareb <shtam.hareb12@gmail.com>
2023-10-23 23:12:18 +0300

    learning props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,11 @@
 import React from "react"
 import "../../src/index.css"
-import star from "../images/star.png"
 export default function Card(props){
     return(
         <div className="card">
          <img src={`../../public/images/${props.img}`} className="card--image" />
             <div className="card--stats">
-                <img src={star} className="card--star" />
+                <img src= className="card--star" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
                 <span className="gray">{props.country}</span>
