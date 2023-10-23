2c642d33afe780001802016c824e95eb124bad65
husseinhareb <shtam.hareb12@gmail.com>
2023-10-23 23:45:28 +0300

    data.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import "../../src/index.css"
 export default function Card(props){
     return(
         <div className="card">
-         <img src={`../../public/images/${props.img}`} className="card--image" />
+         <img src={`/images/${props.img}`} className="card--image" />
             <div className="card--stats">
-                <img src= className="card--star" />
+                <img src="/images/star.png" className="card--star" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
                 <span className="gray">{props.country}</span>
