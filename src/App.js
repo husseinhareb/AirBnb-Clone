20bfe88ef5a84bc3be5cab70205891c952375e07
husseinhareb <shtam.hareb12@gmail.com>
2023-10-24 00:03:32 +0300

    learning props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import data from "./data";
+
 export default function App(){
+  const cards = data.map(item => {
+    return (
+      <Card 
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}
+  />
+    )
+  })
   return(
     <div>
       <Navbar />
-      <Card 
-          img="katie-zaferes.png"
-          rating="5.0"
-          reviewCount={6}
-          country="Usa"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-      />
-      
+      {cards}
     </div>
   )
 }
