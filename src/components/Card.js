95530a1d7ead6ec28f2278d443f844fc296ee633
husseinhareb <shtam.hareb12@gmail.com>
2023-10-28 23:27:44 +0200

    learning props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from "react"
-import "../../src/index.css"
-export default function Card(props){
-    return(
+
+export default function Card(props) {
+    return (
         <div className="card">
-        <div class="card--badge">SOLD OUT</div>
-         <img src={`/images/${props.img}`} className="card--image" />
+            <div className="card--badge">SOLD OUT</div>
+            <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
-                <img src="/images/star.png" className="card--star" />
+                <img src="../images/star.png" className="card--star" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
