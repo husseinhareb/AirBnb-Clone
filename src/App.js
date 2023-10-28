77979000c9f2e1c2d8e3cf8f146ef4d63994a28a
husseinhareb <shtam.hareb12@gmail.com>
2023-10-28 23:46:37 +0200

    learning props

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ export default function App(){
   const cards = data.map(item => {
     return (
       <Card 
-      img={item.coverImg}
-      rating={item.stats.rating}
-      reviewCount={item.stats.reviewCount}
-      country={item.location}
-      title={item.title}
-      price={item.price}
-      openSpots={item.openSpots }
+      key={item.id}
+      item={item}
   />
       )
   })
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,24 @@
 import React from "react"
 
 export default function Card(props) {
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
-            <div className="card--badge">SOLD OUT</div>
-            <img src={`../images/${props.img}`} className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${props.item.coverImg}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
